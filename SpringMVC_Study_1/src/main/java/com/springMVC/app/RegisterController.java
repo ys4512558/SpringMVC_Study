80310827276cc51dfda6e68b900dc2610a90e2d6
Beudicri <ys4512558@naver.com>
2023-08-04 08:16:38 +0900

    Cookie & Session

---
+++ SpringMVC_Study_1/src/main/java/com/springMVC/app/RegisterController.java
@@ -0,0 +1,39 @@
+package com.springMVC.app;
+
+import java.net.URLEncoder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class RegisterController {
+//	@RequestMapping(value="/register/add", method=RequestMethod.GET) // 신규회원 가입
+	@GetMapping("/register/add")
+	public String register() {
+		return "registerForm";  // WEB-INF/views/registerForm.jsp
+	}
+	
+//	@RequestMapping(value="/register/save", method=RequestMethod.POST) // 신규회원 가입
+// 	@PostMapping("/register/save")
+	@PostMapping("/register/add")
+	public String save(@ModelAttribute("user") User user, Model m) {
+		if(!isValid(user)) {
+			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+			
+			m.addAttribute("msg", msg);
+			return "redirect:/register/add"; // 신규회원 가입화면으로 이동(redirect)
+		}
+		
+		return "registerInfo";
+	}
+
+	private boolean isValid(User user) {
+		return false;
+	}
+	
+}
